1dd2a0de1228c9dd23c093648ab4a8fc3956646b
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-03 11:16:30 -0300

    feat: make a header to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import { globalStyles } from "@/styles/global";
+import Image from "next/image";
 import type { AppProps } from "next/app";
+import logoIgniteShop from "../assets/igniteshoplogo.svg";
+import { Container, Header } from "@/styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoIgniteShop} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
