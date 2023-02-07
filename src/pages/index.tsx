7c94fb62e2e773a884128d3df430bf352ad12400
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-07 19:58:13 -0300

    Revert "Revert "feat: delete generic product""
    This reverts commit f15cb5bd620cf1427d52bcc852e653a821bc5a6e.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,20 @@ import camise2 from "../assets/cametas/t-shirt2.png";
 import camise3 from "../assets/cametas/t-shirt3.png";
 
 import "keen-slider/keen-slider.min.css";
+import { stripe } from "@/lib/stripe";
+import { GetServerSideProps } from "next";
+import Stripe from "stripe";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -19,34 +31,40 @@ export default function Home() {
   });
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camise1} alt="" width="520" height="480" />
-        <footer>
-          <strong>Camisa x</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camise2} alt="" width="520" height="480" />
-        <footer>
-          <strong>Camisa x</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camise3} alt="" width="520" height="480" />
-        <footer>
-          <strong>Camisa x</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camise3} alt="" width="520" height="480" />
-        <footer>
-          <strong>Camisa x</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      {products.map((product) => {
+        console.log("images", product.imageUrl);
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} alt="" width="520" height="480" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        );
+      })}
     </HomeContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+    return {
+      id: product.id,
+      name: product.name,
+      imgUrl: product.images[0],
+      price: price.unit_amount / 100,
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
