4bd22e9d324e3aab36acf86636a2ad5524d47eb9
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-09 19:51:03 -0300

    fix: price unit number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ export const getStaticProps: GetStaticProps = async () => {
       price: new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
-      }).format(price.unit_amount / 100),
+      }).format(Number(price.unit_amount) / 100),
     };
   });
 
