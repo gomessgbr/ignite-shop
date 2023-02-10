b88c30f064a6f4104b5dcbb8c602d4276123c48a
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-10 19:08:39 -0300

    feat: fetch PriceId

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,8 +4,9 @@ import {
   ProductContainer,
   ProductDetails,
 } from "@/styles/pages/product";
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import Stripe from "stripe";
 
@@ -16,10 +17,15 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleByProduct() {
+    console.log(product.defaultPriceId);
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -29,16 +35,23 @@ export default function Product({ product }: ProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
         <p>{product.description}</p>
-        <button>Comprar Agora</button>
+        <button onClick={handleByProduct}>Comprar Agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_NIlMQCtrkbrzrO" } }],
+    fallback: "blocking",
+  };
+};
+
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
 }) => {
-  const productsId = params.id;
+  const productsId = params!.id;
 
   const product = await stripe.products.retrieve(productsId, {
     expand: ["default_price"],
@@ -56,6 +69,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(Number(price.unit_amount) / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, //1 hr
