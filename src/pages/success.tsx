da5fada682f0868e3ca09f5bf1ad0861d7ae482a
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-14 18:50:44 -0300

    feat: add success page content

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,17 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
-  return <div>Hello Zica Deu Bom!</div>;
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Gabriel Gomes</strong> , sua
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+      <Link href="">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
