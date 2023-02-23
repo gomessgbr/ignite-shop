5fdd20d33d5be20749e00dbd3cbfe313de5e6473
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-23 16:48:11 -0300

    feat: add CartProvider from use shopping cart lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,24 @@ import Image from "next/image";
 import type { AppProps } from "next/app";
 import logoIgniteShop from "../assets/igniteshoplogo.svg";
 import { Container, Header } from "@/styles/pages/app";
+import { CartProvider } from "use-shopping-cart";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoIgniteShop} alt="" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider
+      mode="payment"
+      cartMode="checkout-session"
+      stripe={process.env.STRIPE_PUBLIC_KEY}
+      currency="USD"
+    >
+      <Container>
+        <Header>
+          <Image src={logoIgniteShop} alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   );
 }
