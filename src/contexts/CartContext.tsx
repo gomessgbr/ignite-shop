b9b596fd4bebbe69ed3380c7acaea0499be3b9d4
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-01 20:01:03 -0300

    feat: make a cart context

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, ReactNode, useState } from "react";
+
+interface ProductProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: string;
+  priceNumber: number;
+  description: string;
+  priceIdDefault: string;
+}
+
+interface CartContextProps {
+  cartItems: ProductProps[];
+  addCart: (product: ProductProps) => void;
+  removeCart: (productId: string) => void;
+  cartTotalPrice: number;
+}
+
+interface CartContextProviderProps {
+  children: ReactNode;
+}
+
+export const CartContext = createContext({} as CartContextProps);
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = useState<ProductProps[]>([]);
+
+  const cartTotalPrice = cartItems.reduce((total, product) => {
+    return total + product.priceNumber;
+  }, 0);
+
+  function addCart(product: ProductProps) {
+    setCartItems((state) => [...state, product]);
+  }
+
+  function removeCart(productId: string) {
+    const filteredCart = cartItems.filter((item) => item.id !== productId);
+    setCartItems(filteredCart);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ addCart, removeCart, cartItems, cartTotalPrice }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
