cf86301f3a7fafd3c5f92392d339f901b5812c36
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-01 19:15:16 -0300

    refactor: root app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,19 @@
 import { globalStyles } from "@/styles/global";
 import Image from "next/image";
 import type { AppProps } from "next/app";
-import logoIgniteShop from "../assets/igniteshoplogo.svg";
-import {
-  Container,
-  Header,
-  HeaderCartWrapper,
-  HeaderCartButton,
-  RoudedCartCount,
-} from "@/styles/pages/app";
+
+import { Container } from "@/styles/pages/app";
 
 import { Handbag } from "phosphor-react";
 import Link from "next/link";
+import { Header } from "@/components/Header";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Header>
-        <Image src={logoIgniteShop} alt="" />
-        <HeaderCartWrapper>
-          <HeaderCartButton>
-            <Handbag size={26} />
-          </HeaderCartButton>
-          <RoudedCartCount>1</RoudedCartCount>
-        </HeaderCartWrapper>
-      </Header>
+      <Header />
       <Component {...pageProps} />
     </Container>
   );
