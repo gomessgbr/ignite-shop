8ddf91a0f5b0019f6ad719029fe828ca6336aaa3
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-02 20:13:53 -0300

    refactor: sucess page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,36 +1,56 @@
-import { stripe } from "@/lib/stripe";
-import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import {
+  ImageContainer,
+  ImageSection,
+  SuccessContainer,
+} from "../styles/pages/success";
 
 interface SuccessProps {
-  costumerName: string;
+  customerName: string;
   product: {
     name: string;
     imageUrl: string;
   };
+  productsImages: string[];
 }
 
-export default function Success({ costumerName, product }: SuccessProps) {
+export default function Success({
+  customerName,
+  productsImages,
+}: SuccessProps) {
   return (
     <>
       <Head>
         <title>Compra efetuada | Ignite Shop</title>
+
         <meta name="robots" content="noindex" />
       </Head>
+
       <SuccessContainer>
-        <h1>Compra Efetuada</h1>
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
+        <h1>Compra efetuada!</h1>
+
+        <ImageSection>
+          {productsImages.map((image, index) => {
+            return (
+              <ImageContainer key={index}>
+                <Image src={image} width={140} height={140} alt="" />
+              </ImageContainer>
+            );
+          })}
+        </ImageSection>
+
         <p>
-          Uhuul <strong>{costumerName}</strong> , sua
-          <strong>{product.name}</strong> já está a caminho da sua casa.
+          Uhuul <strong>{customerName}</strong>, sua compra de{" "}
+          {productsImages.length} camiseta{productsImages.length > 1 && "s"} já
+          está a caminho da sua casa.
         </p>
-        <Link href="">Voltar ao catálogo</Link>
+
+        <Link href="/">Voltar ao catálogo</Link>
       </SuccessContainer>
     </>
   );
@@ -52,16 +72,16 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     expand: ["line_items", "line_items.data.price.product"],
   });
 
-  const costumerName = session.customer_details.name;
-  const product = session.line_items.data[0].price.product as Stripe.Product;
+  const customerName = session?.customer_details?.name;
+  const productsImages = session?.line_items?.data.map((item) => {
+    const product = item?.price?.product as Stripe.Product;
+    return product.images[0];
+  });
 
   return {
     props: {
-      costumerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
-      },
+      customerName,
+      productsImages,
     },
   };
 };
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -7,10 +7,20 @@ export const SuccessContainer = styled("main", {
   justifyContent: "center",
   margin: "0 auto",
   height: 656,
+
+  "@container": {
+    padding: "0 1rem",
+  },
+
+  "@notebook": {
+    minHeight: "calc(100vh - 120px)",
+  },
+
   h1: {
     fontSize: "$2xl",
     color: "$gray100",
   },
+
   p: {
     fontSize: "$xl",
     color: "$gray300",
@@ -23,8 +33,8 @@ export const SuccessContainer = styled("main", {
   a: {
     display: "block",
     marginTop: "5rem",
-    fontSize: "$lg",
     color: "$green500",
+    fontSize: "$lg",
     textDecoration: "none",
     fontWeight: "bold",
 
@@ -34,20 +44,25 @@ export const SuccessContainer = styled("main", {
   },
 });
 
-export const ImageContainer = styled("div", {
-  width: "100%",
-  maxWidth: 130,
-  height: 145,
-  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
-  borderRadius: 8,
-  padding: "0.25rem",
-  marginTop: "4rem",
-
+export const ImageSection = styled("div", {
   display: "flex",
-  alignItems: "center",
+  flexWrap: "wrap",
   justifyContent: "center",
+  marginTop: "4rem",
+});
 
-  img: {
-    objectFit: "cover",
-  },
+export const ImageContainer = styled("div", {
+  position: "relative",
+  zIndex: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "0.25rem",
+  margin: "0 -26px",
+
+  width: 140,
+  height: 140,
+  background: "linear-gradient(180deg, #1ea483 0%, #7465e4 100%)",
+  borderRadius: "50%",
+  boxShadow: "0px 0px 60px rgba(0, 0, 0, 0.8)",
 });
