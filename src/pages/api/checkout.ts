4ed066f50034757bc496d6fb1061aa880792eca5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-02 20:14:04 -0300

    refactor: checkout session

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,32 +1,32 @@
-import { stripe } from "@/lib/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
+import { ProductProps } from "../../contexts/CartContext";
+import { stripe } from "../../lib/stripe";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { priceId } = req.body;
+  const { products } = req.body as { products: ProductProps[] };
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed." });
   }
 
-  if (!priceId) {
+  if (!products) {
     return res.status(400).json({ error: "Price not found." });
   }
 
-  const success_url = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
-  const cancel_url = `${process.env.NEXT_URL}/`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}`;
+
   const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
     mode: "payment",
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
-    success_url: success_url,
-    cancel_url: cancel_url,
+    line_items: products.map((product) => ({
+      price: product.defaultPriceId,
+      quantity: 1,
+    })),
   });
 
   return res.status(201).json({
