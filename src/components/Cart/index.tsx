a0fbfd185a7486142479efa7bd9af450c27ffef3
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-02 19:08:22 -0300

    feat: Cart Component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,5 +1,39 @@
-import { CartContainer } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Handbag, X } from "phosphor-react";
+import { useCart } from "../../hooks/useCart";
+import {
+  ButtonCartLength,
+  ButtonContainer,
+  ModalClose,
+  ModalContent,
+  ModalDescription,
+  ModalOverlay,
+} from "./styles";
 
 export function Cart() {
-  return <CartContainer></CartContainer>;
+  const { cartItems } = useCart();
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <ButtonContainer>
+          <Handbag size={24} weight="bold" color="#8D8D99" />
+
+          {cartItems.length > 0 && (
+            <ButtonCartLength>{cartItems.length}</ButtonCartLength>
+          )}
+        </ButtonContainer>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <ModalOverlay />
+        <ModalContent>
+          <Dialog.Title>Sacola de compras</Dialog.Title>
+          <ModalClose asChild>
+            <X size={24} color="#8D8D99" weight="bold" />
+          </ModalClose>
+        </ModalContent>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 }
