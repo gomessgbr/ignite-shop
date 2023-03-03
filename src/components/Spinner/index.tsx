9a1ee3134412ef5909064ab3f2b092e4f007bca0
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-03 07:52:10 -0300

    fix: spinner container

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,10 +1,11 @@
 import SpinnerImg from "../../assets/spinner.svg";
 import Image from "next/image";
+import { SpinnerContainer } from "./styles";
 
 export function Spinner() {
   return (
-    <>
+    <SpinnerContainer>
       <Image src={SpinnerImg} alt="spiner" width={40} height={40} />
-    </>
+    </SpinnerContainer>
   );
 }
---
+++ src/components/Spinner/styles.ts
@@ -0,0 +1,3 @@
+import { styled } from "@/styles";
+
+export const SpinnerContainer = styled("div", {});
