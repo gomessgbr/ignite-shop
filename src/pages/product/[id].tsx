765ca8436b449275ba28cbcb6a1f6e698be6b9f5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-03 10:05:50 -0300

    fix: title error

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -64,7 +64,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [{ params: { id: "prod_NPaA7NS28zsavE" } }],
-    fallback: true,
+    fallback: "blocking",
   };
 };
 
