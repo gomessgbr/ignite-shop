58d4a11ba0c6815d941554017872d330978a6b58
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-03-03 08:26:17 -0300

    feat: disable fallback

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -64,7 +64,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [{ params: { id: "prod_NIlMQCtrkbrzrO" } }],
-    fallback: "blocking",
+    fallback: false,
   };
 };
 
